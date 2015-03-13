0d1828544232f36b6774bc3ed6731ebcffe301b9
Caio Incau <caioincau@gmail.com>
2015-03-13 15:03:46 -0300

    more gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -56,22 +56,22 @@ gulp.task('html', function() {
 });
 
 gulp.task('css', function(){
-	gulp.src('public/css/*.css')
+	return gulp.src('public/css/*.css')
 	    .pipe(minifyCss())
 	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
 	    .pipe(concat('style.min.css'))
     	.pipe(gulp.dest('dist/public/css'));
 });
 
-return gulp.task('js', function () {
-    gulp.src(['public/js/**/*.js','public/js/*.js'])
+gulp.task('js', function () {
+   return gulp.src(['public/js/**/*.js','public/js/*.js'])
       .pipe(uglify().on('error', gutil.log))
       .pipe(concat('app.js'))
       .pipe(gulp.dest('dist/public/js'));
 });
 
-gulp.task('usemin', ['views','html','css','js'], function() {
-  gulp.src('./app/views/index.ejs')
+ gulp.task('usemin', ['views','html','css','js'], function() {
+  return gulp.src('./app/views/index.ejs')
       .pipe(inject(gulp.src('../.tmp/js/templates.js', {read: false}),
         {
           relative: false,
